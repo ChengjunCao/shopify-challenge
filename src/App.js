17ec7f769b4844243bf2cd774277924fcd1767c8
Mike Cao <mikecao369@gmail.com>
2021-04-30 17:42:34 -0400

    fix deployment

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends React.Component {
   }
 
   fetchData = async() => {
-    const response = await fetch(`https://cors-anywhere.herokuapp.com/http://www.omdbapi.com/?s=${this.state.search}&apikey=d67d7d51`)
+    const response = await fetch(`http://www.omdbapi.com/?s=${this.state.search}&apikey=d67d7d51`)
     const data = await response.json();
     this.setState({ results: data.Search, disabled: [] })
   }
